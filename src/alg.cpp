53440fca285138b6878ad5e1175ada086207721d
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-04-12 20:19:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,14 +1,101 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+#include <stack>
 #include <string>
-#include <map>
-#include "tstack.h"
-
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+#include <climits>
+using namespace std;
+int prec(char c)
+{
+    if (c == '*' || c == '/') {
+        return 3;
+    }
+    if (c == '+' || c == '-') {
+        return 4;
+    }
+    if (c == '&') {
+        return 8;
+    }
+    if (c == '^') {
+        return 9;
+    }
+    if (c == '|') {
+        return 10;
+    }
+    return INT_MAX;
 }
+bool isOperand(char c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+}
+string infx2pstfx(string inf)
+{
+    char probel = ' ';
+    stack<char> s;
+    string postfix;
+    for (char c : inf)
+    {
+        if (c == '(') {
+            s.push(c);
+        }
+        else if (c == ')')
+        {
 
-int eval(std::string pref) {
-  // добавьте код
-  return 0;
+            while (s.top() != '(')
+            {
+                postfix.push_back(s.top());
+                postfix.push_back(probel);
+                s.pop();
+            }
+            s.pop();
+        }
+        else if (isOperand(c)) {
+            postfix.push_back(c);
+            postfix.push_back(probel);
+        }
+        else {
+
+            while (!s.empty() && prec(c) >= prec(s.top()))
+            {
+                postfix.push_back(s.top());
+                s.pop();
+            }
+
+
+            s.push(c);
+        }
+    }
+    while (!s.empty())
+    {
+        postfix.push_back(s.top() );
+        s.pop();
+    }
+    return postfix;
+}
+int eval(string pref)
+{
+    stack<int> stack;
+    for (char c : pref)
+    {
+        if (c >= '0' && c <= '9') {
+            stack.push(c - '0');
+        }
+        else {
+            int x = stack.top();
+            stack.pop();
+            int y = stack.top();
+            stack.pop();
+            if (c == '+') {
+                stack.push(y + x);
+            }
+            else if (c == '-') {
+                stack.push(y - x);
+            }
+            else if (c == '*') {
+                stack.push(y * x);
+            }
+            else if (c == '/') {
+                stack.push(y / x);
+            }
+        }
+    }
+    return stack.top();
 }

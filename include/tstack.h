d66424624d95b8b809d4e335e6d9359aa4c93414
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-04-12 20:40:44 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
+#include <stack>
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  std::stack <T> stack;
 };
 
 #endif  // INCLUDE_TSTACK_H_

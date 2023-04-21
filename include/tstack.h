697c64ebfb261a9dde67715dcdc2bc49cdf26b05
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-04-21 13:26:59 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+#include <string>
 template<typename T, int size>
 class TStack {
-    std::stack <T> stack;
-};
+private:
+    T arr[100];
+    int m;
 
+public:
+    TStack() : m(-1) {}
+    void push(T value) {
+        if (!isFull())
+            arr[++m] = value;
+        else
+            throw std::string("Full");
+    }
+    T get() const {
+        return arr[m];
+    }
+    T pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[m--];
+    }
+    bool isEmpty() const {
+        return m == -1;
+    }
+    bool isFull() const {
+        return m == size - 1;
+    }
+    T top() {
+        if (!isEmpty()) {
+            return arr[m];
+        }
+        else {
+            exit(EXIT_FAILURE);
+        }
+    }
+};
 #endif  // INCLUDE_TSTACK_H_
